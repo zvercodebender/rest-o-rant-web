68bc4e9010c5d50dcc50a4b2716da0e196ace62b
Admin <rbroker@digital.ai>
2021-02-11 12:45:45 -0500

    change yaml file name

--- deploy_package.sh
+++ deploy_package.sh
@@ -13,11 +13,11 @@ sed "s/{{VERSION}}/${VERSION}/g" src/yaml/artifacts/rest-o-rant-web.yaml > build
 cd build/yaml
 if [ -z "${GET_LOCAL}" ]
 then
-  xl apply --file rest-o-rant-web.yaml --values version=$VERSION
+  xl apply --file web.yaml --values version=$VERSION
 else
   curl -LO https://dist.xebialabs.com/public/xl-cli/$CLI_VERSION/linux-amd64/xl
   chmod +x xl
-  ./xl apply --xl-deploy-url=$XLD_URL --xl-deploy-username=$XLD_USER --xl-deploy-password=$XLD_PASSWD --file rest-o-rant-web.yaml --values version=$VERSION
+  ./xl apply --xl-deploy-url=$XLD_URL --xl-deploy-username=$XLD_USER --xl-deploy-password=$XLD_PASSWD --file web.yaml --values version=$VERSION
   rm xl
 fi
 cd ../..
