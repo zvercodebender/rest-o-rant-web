bf85625ae3884e220452ad183771e8b85fa9eac1
Rick Broker <rbroker@cloudbees.com>
2022-03-30 11:24:59 -0400

    Update docker image

--- deploy_package.sh
+++ deploy_package.sh
@@ -8,16 +8,4 @@ then
 fi
 echo "Version = ${VERSION}"
 
-cp -R src/yaml build/yaml
-sed "s/{{VERSION}}/${VERSION}/g" src/yaml/artifacts/rest-o-rant-web.yaml > build/yaml/artifacts/rest-o-rant-web.yaml
-cd build/yaml
-if [ -z "${GET_LOCAL}" ]
-then
-  xl apply --file web.yaml --values version=$VERSION
-else
-  curl -LO https://dist.xebialabs.com/public/xl-cli/$CLI_VERSION/linux-amd64/xl
-  chmod +x xl
-  ./xl apply --xl-deploy-url="${XLD_URL}" --xl-deploy-username="${XLD_USER}" --xl-deploy-password="${XLD_PASSWD}" --file web.yaml --values version=$VERSION
-  rm xl
-fi
-cd ../..
+
